0ec38b0781cd2996b85d132d217806177cd327b7
Harisaipravin <harisaipravin@gmail.com>
2020-04-11 11:11:39 +0530

    MinimifyURL

--- scrap.py
+++ scrap.py
@@ -1,5 +1,34 @@
 
+from bs4 import BeautifulSoup
+import requests
 
+def getURL(data):
+    
+    coun = data.find("www.amazon.")
+    length = len(data)
+    if coun != -1:
+        ins = data.find("/dp/")
+        if ins != -1:
+            ine = ins + 14
+            temp = data[ins:ine]
+        else:
+            ins = data.find("/gp/")
+            if ins != -1:
+                ine = ins + 22
+                temp = data[ins:ine]
+            else:
+                return None
+            
+        short = data[coun:length]
+        fi = short.find("/")
+        url = "https://" + short[:fi] + temp
+        #print(url)
+    
+    else:
+        url = None
+    
+    return url
+        
 def getExt(data):
     
     ind = data.find("www.amazon.")
@@ -10,4 +39,3 @@ def getExt(data):
     #print(ext)
     
     return ext
- 
