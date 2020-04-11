96ba47993aafe20c516da1d4c01a2ba50b1738a0
Harisaipravin <harisaipravin@gmail.com>
2020-04-11 11:13:24 +0530

    getProductDetails

--- scrap.py
+++ scrap.py
@@ -4,7 +4,7 @@ from price_parser import Price
 import requests
 
 def getURL(data):
-    
+
     coun = data.find("www.amazon.")
     length = len(data)
     if coun != -1:
@@ -51,3 +51,37 @@ def getCurrency(cost):
     price = Price.fromstring(cost)
     #print(price.currency)
     return price.currency
+
+
+def getProdDetails(data):
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36"
+    }
+    details = {"Name": "",
+               "Price": 0,
+               "Currency": None ,
+               "Deal": True,
+               "Url": ""
+               };
+               
+    url = getURL(data)
+    if url is None:
+        details = None
+    else:
+        page = requests.get(url, headers=headers)
+        soup = BeautifulSoup(page.content, "html5lib")
+        title = soup.find(id="productTitle")
+        price = soup.find(id="priceblock_dealprice")
+        if price is None:
+            price = soup.find(id="priceblock_ourprice")
+            details["Deal"] = False
+        if title is not None and price is not None:
+            details["Name"] = title.get_text().strip()
+            details["Price"] = getPrice(price.get_text())
+            details["Currency"]=getCurrency(price.get_text())
+            details["Url"] = url
+        else:
+            details = None
+    print(details)
+    #return details
