fe2510fdceab8b300b56a4bb7fbeb20567da30f8
Harisaipravin <harisaipravin@gmail.com>
2020-04-12 09:34:56 +0530

    AmazonEye:Minor updates

--- mongo.py
+++ mongo.py
@@ -2,7 +2,7 @@ import datetime
 import pymongo
 import scrap
 
-client = pymongo.MongoClient("mongodb+srv://<user>:<pass>@scrapbook-iydc2.gcp.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://amazon:tracker@scrapbook-iydc2.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = client.Amz
 
 def pushData(details,URL):
@@ -10,10 +10,10 @@ def pushData(details,URL):
     ext=scrap.getExt(URL)
     
     new = db[ext]
-    ASIN = details["url"][len(details["url"])-10:len(details["url"])]
+    ASIN = details["Url"][len(details["Url"])-10:len(details["Url"])]
 
     try:
-        details["date"] = datetime.datetime.utcnow()
+        details["Date"] = datetime.datetime.utcnow()
         new.update_one({"asin":ASIN}, {"$set": {"asin":ASIN}, "$push":{"details":details}}, upsert=True)
         return True
     except Exception as err:
