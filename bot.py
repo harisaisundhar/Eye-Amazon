79bee8cbef3ca46591df3e9e47297c77d9b924e8
Harisaipravin <harisaipravin@gmail.com>
2020-04-13 14:45:41 +0530

    EyeAmazon:TelegramBotSync

---
+++ bot.py
@@ -0,0 +1,49 @@
+import logging
+import os
+import random
+import sys
+import track
+
+from telegram.ext import Updater, CommandHandler
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
+mode = os.getenv("prod")
+TOKEN = os.getenv("<TOKEN>")
+if mode == "dev":
+    def run(updater):
+        updater.start_polling()
+elif mode == "prod":
+    def run(updater):
+        PORT = int(os.environ.get("PORT", "8443"))
+        HEROKU_APP_NAME = os.environ.get("<HEROKU_APP_NAME>")
+        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
+        updater.start_webhook(listen="0.0.0.0",
+                              port=PORT,
+                              url_path=TOKEN)
+        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+else:
+    logger.error("No MODE specified!")
+    sys.exit(1)
+
+
+def start_handler(bot, update):
+    logger.info("User {} started bot".format(update.effective_user["id"]))
+    update.message.reply_text("Hello from Python!\nPress /random to get random number")
+
+
+def track_handler(bot, update):
+    logger.info("User {} Tracking bot".format(update.effective_user["id"]))
+    update.message.reply_text("telgram bot in development")
+
+
+if __name__ == '__main__':
+    logger.info("Starting bot")
+    updater = Updater(TOKEN)
+
+    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
+    updater.dispatcher.add_handler(CommandHandler("track", track_handler))
+
+    run(updater)
